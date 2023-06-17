e63b9637bf0f53585d42c4aeecc366750b518b8c
Sanjay2929 <sanjayjyani220@gmail.com>
2023-06-17 16:01:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Nisi } from "./components/Nisi";
-import HeroSec from "./components/HeroSec";
 import Contact from "./components/Contact";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import NavigationBar from "./components/NavigationBar";
 import { Nisi } from "./components/Nisi";
 import { Portfolios } from "./components/Portfolios";
+import HeroSec from "./components/HeroSec";
 
 function App() {
   return (
     <div className=" overflow-hidden">
-      <NavigationBar />
+      <HeroSec />
       <Nisi />
       <Portfolios />
-      <Contact/>
+      <Contact />
     </div>
   );
 }
