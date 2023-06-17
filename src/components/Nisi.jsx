9cd831a7d37da1d5154bdfe06bf7406a0c89b850
sukhvinder-jaat <monudhaka829@gmail.com>
2023-06-17 12:43:23 +0530

    btn

--- src/components/Nisi.jsx
+++ src/components/Nisi.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap-v5";
 import side_img from "../assets/img/svg/section_2_side_img.svg";
+import { OrangeBtn } from "./OrangeBtn";
 export const Nisi = () => {
   return (
     <div className="nisi_bg py-5 position-relative">
@@ -12,8 +13,8 @@ export const Nisi = () => {
           <Row className="py-5">
             <Col lg={8} xs={12}>
               <p className="mb-0 ff_roy_bold text-uppercase fw_900 fs_lg clr_black_07 nisi_text">
-                <span className=" d-xl-block">Nisi et diam euismod </span>suspendisse
-                tristique quis
+                <span className=" d-xl-block">Nisi et diam euismod </span>
+                suspendisse tristique quis
               </p>
             </Col>
             <Col lg={4} xs={12} className=" pt-lg-0 pt-4">
@@ -21,9 +22,7 @@ export const Nisi = () => {
                 Tincidunt phasellus id imperdiet lorem blandit. Congue donec
                 scelerisque congue pellentesque.
               </p>
-              <button className="us_btn rounded-pill clr_white fw_700 ff_medium fs_sm">
-                Contact Us
-              </button>
+              <OrangeBtn text=" Contact Us" />
             </Col>
           </Row>
         </Container>
