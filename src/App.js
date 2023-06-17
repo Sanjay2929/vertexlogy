855bfb57740860cff733093e0e33154faf7f943e
sukhvinder-jaat <monudhaka829@gmail.com>
2023-06-17 15:43:00 +0530

    slider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import NavigationBar from "./components/NavigationBar";
 import { Nisi } from "./components/Nisi";
+import { Portfolios } from "./components/Portfolios";
 
 function App() {
   return (
-    <>
+    <div className=" overflow-hidden">
       <NavigationBar />
-      <Nisi/>
-    </>
+      <Nisi />
+      <Portfolios />
+    </div>
   );
 }
 
