61a7533bd1f689ed4fe345a042c3640a9527774f
sukhvinder-jaat <monudhaka829@gmail.com>
2023-06-17 09:52:01 +0530

    root

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import NavigationBar from "./components/NavigationBar";
+import { Nisi } from "./components/Nisi";
 
 function App() {
   return (
     <>
       <NavigationBar />
+      <Nisi/>
     </>
   );
 }
---
+++ src/components/Nisi.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Nisi = () => {
+  return (
+    <div>Nisi</div>
+  )
+}
