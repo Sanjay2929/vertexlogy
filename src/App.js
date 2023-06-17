2c5a922b718f1804e4184e0d380dfbc1a86a2db0
Sanjay2929 <sanjayjyani220@gmail.com>
2023-06-17 09:30:53 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
     <>
-      
+      <NavigationBar />
     </>
   );
 }
---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from 'react-bootstrap-v5'
+
+const NavigationBar = () => {
+  return (
+      <div>
+          <nav>
+              <Container>
+                 
+              </Container>
+          </nav>
+    </div>
+  )
+}
+
+export default NavigationBar
